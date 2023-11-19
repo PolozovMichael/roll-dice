e06f6b7b658cbd8a4ec24c997f3212a01c2509c8
Erik <erik02003@mail.ru>
2023-11-19 17:50:41 +0600

    first commit

--- server.js
+++ server.js
@@ -1,61 +1,86 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const client = require('prom-client');
+const client = require("prom-client");
 
 let register = new client.Registry();
 
-const headsCount = new client.Counter({
-    name: "heads_count",
-    help: "Number of heads"
+const oneCount = new client.Counter({
+  name: "one_count",
+  help: "Number of ones",
 });
 
-const tailsCount = new client.Counter({
-    name: "tails_count",
-    help: "Number of tails"
+const twoCount = new client.Counter({
+  name: "two_count",
+  help: "Number of twos",
 });
 
-const flipCount = new client.Counter({
-    name: "flip_count",
-    help: "Number of flips"
+const threeCount = new client.Counter({
+  name: "three_count",
+  help: "Number of threes",
 });
 
-register.registerMetric(headsCount);
-register.registerMetric(tailsCount);
-register.registerMetric(flipCount);
+const fourCount = new client.Counter({
+  name: "four_count",
+  help: "Number of fours",
+});
+
+const fiveCount = new client.Counter({
+  name: "five_count",
+  help: "Number of fives",
+});
+
+const sixCount = new client.Counter({
+  name: "six_count",
+  help: "Number of sixes",
+});
+
+const rollCount = new client.Counter({
+  name: "roll_count",
+  help: "Number of rolls",
+});
+
+register.registerMetric(oneCount);
+register.registerMetric(twoCount);
+register.registerMetric(threeCount);
+register.registerMetric(fourCount);
+register.registerMetric(fiveCount);
+register.registerMetric(sixCount);
+register.registerMetric(rollCount);
 
 register.setDefaultLabels({
-    app: 'coin-api'
+  app: "dice-api",
 });
 
 client.collectDefaultMetrics({ register });
 
-app.get('/flip-coins', (request, response) => {
-    const times = request.query.times;
-    if (times && times > 0) {
-        flipCount.inc(Number(times));
-        let heads = 0;
-        let tails = 0;
-        for (let i = 0; i < times; i++) {
-            let randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                heads++;
-            } else {
-                tails++;
-            }
-        }
-        headsCount.inc(heads);
-        tailsCount.inc(tails);
-        response.json({ heads, tails });
-    } else {
-        response.send('hello! i work!!');
+app.get("/roll-dice", (request, response) => {
+  const times = request.query.times;
+  if (times && times > 0) {
+    rollCount.inc(Number(times));
+    let counts = [0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < times; i++) {
+      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      counts[randomNumber - 1]++;
     }
+
+    oneCount.inc(counts[0]);
+    twoCount.inc(counts[1]);
+    threeCount.inc(counts[2]);
+    fourCount.inc(counts[3]);
+    fiveCount.inc(counts[4]);
+    sixCount.inc(counts[5]);
+
+    response.json({ counts });
+  } else {
+    response.send("Specify the number of times to roll the dice.");
+  }
 });
 
-app.get('/metrics', async (request, response) => {
-    response.setHeader('Content-type', register.contentType);
-    response.end(await register.metrics());
+app.get("/metrics", async (request, response) => {
+  response.setHeader("Content-type", register.contentType);
+  response.end(await register.metrics());
 });
 
 app.listen(5000, () => {
-    console.log('Started server. Listening on port 5000.');
+  console.log("Started server. Listening on port 5000.");
 });
